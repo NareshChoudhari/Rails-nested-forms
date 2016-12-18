b6c54b21e0e1985281972f15080be605c39cf1a4
blackcoat77 <kimanovic@gmail.com>
2016-12-17 23:26:09 -0500

    Finishing scaffolding,  Add validates to both models and to_s method

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,3 +1,8 @@
 class Father < ApplicationRecord
   has_many :sons
+  validates :f_name, presence: true
+
+  def to_s
+   f_name
+  end
 end
--- app/models/son.rb
+++ app/models/son.rb
@@ -1,3 +1,8 @@
 class Son < ApplicationRecord
   belongs_to :father
+  validates :s_name, presence: true
+
+  def to_s
+   s_name
+  end
 end
