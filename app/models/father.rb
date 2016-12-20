493272393a2150de29772ba0f763b94dcc96ed0f
blackcoat77 <kimanovic@gmail.com>
2016-12-20 14:38:05 -0500

    Adding inverse_of: for both models and allow_blank: true for allowing blank/nil objects

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,8 +1,11 @@
 class Father < ApplicationRecord
-  has_many :sons
+  has_many :sons, inverse_of: :father
   validates :f_name, presence: true
-  accepts_nested_attributes_for :sons,
-                    reject_if: proc { |attributes| attributes['s_name'].blank? }
+  accepts_nested_attributes_for :sons
+
+  # This code below rejects child attribute for validation, but not saving into DB
+  # accepts_nested_attributes_for :sons,
+                    # reject_if: proc { |attributes| attributes['s_name'].blank? }
 
   def to_s
    f_name
--- app/models/son.rb
+++ app/models/son.rb
@@ -1,8 +1,19 @@
 class Son < ApplicationRecord
-  belongs_to :father
-  validates :s_name, presence: true
+  belongs_to :father, inverse_of: :sons
+  validates :s_name, presence: true, allow_blank: true
 
   def to_s
    s_name
   end
 end
+# class Son < ApplicationRecord
+#   belongs_to :father, inverse_of: :sons
+#   validates :s_name, presence: false  # works
+#
+# # validates :s_name, presence: true, allow_blank: true # this works too
+  # validates :s_name, :presence => { :allow_blank => true } # this works
+#
+#   def to_s
+#    s_name
+#   end
+# end
